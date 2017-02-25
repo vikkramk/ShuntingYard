dcc22314cb352c7f02be0b99b87e92ce6653416f
vik <vikram@sharky.home>
2017-02-24 20:43:26 -0800

    finished

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
+/*Implementation of the Shunting Yard Algorithm
+* Converts infix to postfix notation
+* Uses a linked list stack
+* Enter a line with spaces between tokens
+*/
+
 #include <iostream>
 #include <cctype>
 #include <cstddef>
+#include <cstring>
 
 
-//Struct for char holding node
+//Struct for char-holding node
 struct Node {
 	Node (char data) {
 		next = NULL;
@@ -31,33 +38,42 @@ Stack::Stack() {
 }
 
 Stack::~Stack() {
+	//Delete the nodes
 	while (head != NULL) {
-		Node next = head->next;
+		Node* next = head->next;
 		delete head;
-		head = ref;
+		head = next;
 	}
 }
 
+//Pop from the head
 char Stack::pop() {
+	if (head == NULL)
+		return '\0';
 	char c = head->data;
 	head = head->next;
 	return c;
 }
 
+//Push by making a new head
 void Stack::push(char c) {
 	Node* newhead = new Node(c);
 	newhead->next = head;
 	head = newhead;
 }
 
+//Peek at head
 char Stack::peek() {
-	return head->data;
+	if (head != NULL)
+		return head->data;
+	else
+		return '\0';
 }
 
 
-//Get the operator precedence for an operator
+//Get the precedence for an operator from a lookup table
 int opPrecedence(char c) {
-	char operators[256] = {};
+	char operators[256] = {0};
 
 	if (c > 256)
 		return 0;
@@ -71,33 +87,63 @@ int opPrecedence(char c) {
 	return operators[c];
 }
 
-
 //Main function
 int main() {
-	char tokens[80];
-	Stack stack = new Stack();
+	Stack ops;
+	char line[80];
+	char* lineptr = line;
+	bool finished = false;
 
-	std::cin >> tokens;
-	while (*tokens++ != '\0') {	//Iterate through characters
-		if (isdigit(*tokens)) {
-			std::cout << *tokens;
+	std::cin.getline(line, 80);
+
+	while (!finished) {
+		//Get the next token
+		char token[80];
+		int i;
+		for (i = 0; *lineptr != ' ' && *lineptr != '\0'; i++, lineptr++) {
+			token[i] = *lineptr;
 		}
-		else if (opPrecedence(*tokens)!=0) {
-			while (opPrecedence(stack->peek()) > opPrecedence(*tokens)) {
-				std::cout << stack->pop();
+		token[i] = '\0';
+
+		//Set to be done if reaching end of line
+		finished = (*(lineptr++) == '\0');
+
+		//Print numbers
+		if (isdigit(token[0])) {
+			std::cout << token << std::flush;
+		}
+		//Deal with an operator
+		else if (opPrecedence(token[0])!=0) {
+			//Print all ops on stack with a higher precedence
+			while (opPrecedence(ops.peek()) > opPrecedence(token[0])) {
+				std::cout << ops.pop() << std::flush;
 			}
-			stack->push(*tokens);
+			//Push this operator
+			ops.push(token[0]);
 		}
-		if (tokens* == '(') {
-			stack->push(*tokens);
+		//Push if a left paren
+		if (token[0] == '(') {
+			ops.push(*token);
 		}
-		if (tokens* == ')') {
-			while (stack->peek() != '(')
-				std::cout << stack->pop();
-			stack->pop();
+		//Deal with right paren
+		if (token[0] == ')') {
+			//Pop all ops until the left paren
+			while (ops.peek() != '(')
+				std::cout << ops.pop() << std::flush;
+			//Dispose of left paren
+			ops.pop();
 		}
-		while (stack->peek() != NULL) {
-				std::cout << stack->pop();
-		}
+
+		//Space the tokens
+		std::cout << " " << std::flush;
 	}
+
+	//Print rest of the ops of stack
+	while (ops.peek() != '\0') {
+		std::cout << ops.pop() << " " << std::flush;
+	}
+
+	std::cout << std::endl;	//Newline
+
+	return 0;
 }
